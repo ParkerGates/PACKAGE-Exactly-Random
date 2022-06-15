9c704589eaaea36ea5f89bd7369fc259f488942c
Parker Gates <parkergates14@gmail.com>
2022-06-15 07:38:26 -0600

    randRange, rand0ToMax, rand1ToMax, randDivisable0ToMax, randDivisableToMax functions added. isDivisable helper function added

--- index.js
+++ index.js
@@ -4,7 +4,33 @@ class ExactlyRandom {
         console.log("working");
     }
 }
-
-ExactlyRandom.test();
-
 module.exports = ExactlyRandom;
+
+
+const randRange = (min, max) => Math.floor(Math.random() * ((max + 1) - min) + min);
+const rand0ToMax = (max) =>  Math.floor((Math.random() * max) + 1);
+const rand1ToMax = (max) => Math.floor(Math.random() * (max + 1));
+const randDivisable0ToMax = (max, divisableBy) => isDivisable({max:max,divisable:divisableBy},(Math.floor(Math.random() * ((max / divisableBy) + 1)) * divisableBy));
+const randDivisableToMax = (max, divisableBy) => isDivisable({max:max,divisable:divisableBy},(Math.floor((Math.random() * (max / divisableBy)) + 1) * divisableBy));
+
+const isDivisable = ({max, divisable, min = null}, equation) => max % divisable === 0 && (min % divisable === 0 || min === null) ? equation : null;
+
+
+// const randRangeDivisable = (min, max, divisable) => {
+//     test = Math.floor(Math.random() * ((max + 1) - min) + min     );
+//     console.log(test);
+//     return test;
+// };
+
+console.log(randDivisable0ToMax(10, 2));
+console.log(randDivisable0ToMax(10, 3));
+
+let max = 50;   
+let x = () => Math.floor(Math.random()*max/10)*10   +10;
+                //rand between  / divisable = 5 * 10
+
+
+
+for (let i = 0; i < 8; i++) {
+    
+}
